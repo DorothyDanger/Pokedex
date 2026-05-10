3d0adbeefdc87997f6887fa469b6329a9c5cfdef
DorothyDanger <harriotpower@hotmail.com>
2026-05-10 01:38:25 +0100

    Changed the main function from hello world. It now serves as an REPL.
    Updated .gitignore to ignore the repl.log file.

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	//infinite for loop to read user input until they exit
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			input := scanner.Text()
+			splitInput := cleanInput(input)
+			fmt.Printf("Your command was: %s\n", splitInput[0])
+		}
+
+	}
 }
